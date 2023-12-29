54197a858bb70f97d0069a79faebd526578b5f5b
SandrLite <mr.wlas@mail.ru>
2023-12-29 22:29:27 +0300

    Получаем строку от пользователя. Затем создаем массив из строки.

--- MainBlock/Program.cs
+++ MainBlock/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MainBlock
 {
@@ -10,6 +7,44 @@ namespace MainBlock
 	{
 		static void Main(string[] args)
 		{
+			int count = 0;
+			string[] arr1;
+			do
+			{
+				arr1 = createArray(reader());
+				count = arr1.Count();
+				if(count < 6)
+				{
+					Console.WriteLine("Ошибка, вы ввели меньше 6 слов.");
+				}
+			}
+			while (count < 6);			
 		}
+		/// <summary>
+		/// Получаем строку из 6 слов от пользователя
+		/// </summary>
+		/// <returns>Возвращается строка</returns>
+		private static string reader()
+		{
+			string input;
+			do
+			{
+				Console.Write("Введите 6 любых слов через пробел: ");
+				input = Console.ReadLine();
+			}
+			while (string.IsNullOrEmpty(input));
+
+			return input;
+		}
+		/// <summary>
+		/// Метод создает массив из строки
+		/// </summary>
+		/// <param name="str">Строка полученная от пользователя</param>
+		/// <returns>Массив из строки</returns>
+		private static string[] createArray(string str)
+		{
+			return str.Split(' ');
+		}
+
 	}
 }
